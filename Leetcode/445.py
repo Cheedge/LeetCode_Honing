3bd4df27199593bec27433c97d7f0e3b00b198ee
Cheedge <cheedgelee@gmail.com>
2022-07-02 11:44:23 +0200

    LinkedList

---
+++ Leetcode/445.py
@@ -0,0 +1,68 @@
+"""
+445. Add Two Numbers II
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers.
+The most significant digit comes first and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Follow up: Could you solve it without reversing the input lists?
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        num1, num2 = "", ""
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        # print(num1, num2)
+        num = eval(num1+'+'+num2)
+        tot = str(num)
+        head = ListNode(val=int(tot[0]))
+        ptr = head
+        tot = tot[1:]
+        # tot = tot.lstrip(tot[0])
+        while ptr and len(tot) > 0:
+            # print(tot)
+            ptr.next = ListNode(val=int(tot[0]))
+            ptr = ptr.next
+            tot = tot[1:]
+            # tot = tot.lstrip(tot[0])
+        return head
