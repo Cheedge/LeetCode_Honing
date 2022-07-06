c134990f52397ee3736bb9ec48142e2651b6a2c4
Cheedge <cheedgelee@gmail.com>
2022-07-06 14:29:20 +0200

    DP LCS

--- Leetcode/583.py
+++ Leetcode/583.py
@@ -52,6 +52,42 @@ def minDistance(word1: str, word2: str) -> int:
             return 1 + min(lcs(w1, w2, i+1, j), lcs(w1, w2, i, j+1))
     return lcs(word1, word2, 0, 0)
 
+def minDistance_lcs(word1, word2):
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: int
+    """
+    # LCS
+    m, n = len(word1), len(word2)
+    # dp[i][j]: lcs of word1[i:] and word2[j:]
+    dp = [[-1 for j in range(n)] for i in range(m)]
+    
+    def recursion(i, j, dp):
+        if i==m-1 or j==n-1:
+            if word1[i]==word2[j]:
+                return 1
+            else:
+                if i!=m-1:
+                    return recursion(i+1, j, dp)
+                elif j!=n-1:
+                    return recursion(i, j+1, dp)
+                else:
+                    return 0
+        # print(i, j, m, n, len(dp), len(dp[0]))
+        if dp[i][j]!=-1: return dp[i][j]
+        if word1[i]==word2[j]:
+            dp[i][j] = recursion(i+1, j+1, dp) + 1
+        else:
+            dp[i][j] = max(recursion(i+1, j, dp), recursion(i, j+1, dp), recursion(i+1, j+1, dp))
+        # print(dp)
+        return dp[i][j]
+    
+    return m + n - 2 * recursion(0, 0, dp)
+
+
+
+
 test_case = [
     ("seaat", "eats", 3),
     ("leetcode", "etco", 4),
@@ -61,3 +97,7 @@ test_case = [
 @pytest.mark.parametrize("word1, word2, expect", test_case)
 def test_minDistance(word1: str, word2: str, expect: int)->None:
     assert minDistance(word1, word2) == expect
+
+@pytest.mark.parametrize("word1, word2, expect", test_case)
+def test_minDistance_lcs(word1: str, word2: str, expect: int)->None:
+    assert minDistance_lcs(word1, word2) == expect
