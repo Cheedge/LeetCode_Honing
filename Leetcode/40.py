513629543b23f4cfa01285f1b82923ba56d495af
Cheedge <cheedgelee@gmail.com>
2022-07-10 17:58:58 +0200

    backtracking

--- Leetcode/40.py
+++ Leetcode/40.py
@@ -37,3 +37,26 @@ Constraints:
 1 <= candidates[i] <= 50
 1 <= target <= 30
 """
+from typing import List
+
+
+def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
+    candidates.sort()
+    n, path, res = len(candidates), list(), list()
+    
+    def recursion(idx, remain, path, res):
+        if remain==0:
+            res.append(path)
+            return
+        if remain<0:
+            return
+        for i in range(idx, n):
+            # [1,1,1,6,7], target=8 -> to avoid [1,7],[1,7][1,7] kind of duplications
+            if i>idx and candidates[i]==candidates[i-1]:
+                continue
+            else:
+                # but the [1,1,6] situation is achieved by recursion.
+                recursion(i+1, remain-candidates[i], path+[candidates[i]], res)
+        # return res
+    recursion(0, target, path, res)
+    return res
