d890d37a3045528173cb0ea81b6749e35c348c25
Cheedge <cheedgelee@gmail.com>
2022-08-03 11:02:05 +0200

    Binary Search

--- Leetcode/350.py
+++ Leetcode/350.py
@@ -7,7 +7,6 @@ return an array of their intersection.
 Each element in the result must appear
 as many times as it shows in both arrays and you may return the result in any order.
 
- 
 
 Example 1:
 
@@ -18,13 +17,13 @@ Example 2:
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 Output: [4,9]
 Explanation: [9,4] is also accepted.
- 
+
 
 Constraints:
 
 1 <= nums1.length, nums2.length <= 1000
 0 <= nums1[i], nums2[i] <= 1000
- 
+
 
 Follow up:
 
@@ -50,6 +49,7 @@ def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
                 res.append(it)
     return res
 
+
 def intersect_bs(nums1, nums2):
     """
     :type nums1: List[int]
@@ -60,7 +60,7 @@ def intersect_bs(nums1, nums2):
     nums2.sort()
     n1, n2 = len(nums1), len(nums2)
     res = list()
-    if n1<n2:
+    if n1 < n2:
         for i in range(n1):
             j = bisect.bisect_left(nums2, nums1[i])
             # print(j, n2)
@@ -72,5 +72,5 @@ def intersect_bs(nums1, nums2):
             j = bisect.bisect_left(nums1, nums2[i])
             if j < len(nums1) and nums1[j] == nums2[i]:
                 res.append(nums2[i])
-                nums1.remove(nums2[i])    
+                nums1.remove(nums2[i])
     return res
