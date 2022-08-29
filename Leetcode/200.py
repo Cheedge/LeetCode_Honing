893e31b69257eaa43f5bfe2cd3a5dcf4258a0648
Cheedge <cheedgelee@gmail.com>
2022-08-29 13:00:04 +0200

    BFS, DFS

--- Leetcode/200.py
+++ Leetcode/200.py
@@ -8,7 +8,7 @@ return the number of islands.
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
 You may assume all four edges of the grid are all surrounded by water.
 
- 
+
 
 Example 1:
 
@@ -28,7 +28,7 @@ Input: grid = [
   ["0","0","0","1","1"]
 ]
 Output: 3
- 
+
 
 Constraints:
 
@@ -40,6 +40,7 @@ grid[i][j] is '0' or '1'.
 from collections import deque
 
 
+# BFS
 def numIslands(grid):
     """
     :type grid: List[List[str]]
@@ -58,20 +59,52 @@ def numIslands(grid):
                 cnt += 1
                 while dq:
                     a, b = dq.pop()
-                    if a+1<m:
-                        if grid[a+1][b]=="1" and (a+1, b) not in repo:
-                            dq.append((a+1, b))
-                            repo.add((a+1, b))
-                    if a-1>=0:
-                        if grid[a-1][b]=="1" and (a-1, b) not in repo:
-                            dq.append((a-1, b))
-                            repo.add((a-1, b))
-                    if b+1<n:
-                        if grid[a][b+1]=="1" and (a, b+1) not in repo:
-                            dq.append((a, b+1))
-                            repo.add((a, b+1))
-                    if b-1>=0:
-                        if grid[a][b-1]=="1" and (a, b-1) not in repo:
-                            dq.append((a, b-1))
-                            repo.add((a, b-1))
+                    if a + 1 < m:
+                        if grid[a + 1][b] == "1" and (a + 1, b) not in repo:
+                            dq.append((a + 1, b))
+                            repo.add((a + 1, b))
+                    if a - 1 >= 0:
+                        if grid[a - 1][b] == "1" and (a - 1, b) not in repo:
+                            dq.append((a - 1, b))
+                            repo.add((a - 1, b))
+                    if b + 1 < n:
+                        if grid[a][b + 1] == "1" and (a, b + 1) not in repo:
+                            dq.append((a, b + 1))
+                            repo.add((a, b + 1))
+                    if b - 1 >= 0:
+                        if grid[a][b - 1] == "1" and (a, b - 1) not in repo:
+                            dq.append((a, b - 1))
+                            repo.add((a, b - 1))
     return cnt
+
+
+# DFS
+def numIslands1(grid):
+    m, n = len(grid), len(grid[0])
+    visited = set()
+    res = 0
+
+    def DFS(i, j, visited):
+        if i < 0 or j < 0 or i >= m or j >= n:
+            return False
+        if (i, j) in visited:
+            if grid[i][j] == "1":
+                return True
+            else:
+                return False
+        if grid[i][j] == "1":
+            visited.add((i, j))
+            DFS(i - 1, j, visited) | DFS(i + 1, j, visited) | DFS(
+                i, j - 1, visited
+            ) | DFS(i, j + 1, visited)
+            return True
+        else:
+            return False
+
+    for i in range(m):
+        for j in range(n):
+            if (i, j) in visited:
+                continue
+            if DFS(i, j, visited):
+                res += 1
+    return res
