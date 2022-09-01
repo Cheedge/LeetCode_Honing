a703e6e5e85d2f6c5507c47c9f38db769b8d5f12
Cheedge <cheedgelee@gmail.com>
2022-09-01 23:31:00 +0200

    BFS

--- Leetcode/1091.py
+++ Leetcode/1091.py
@@ -13,7 +13,7 @@ All the adjacent cells of the path are 8-directionally connected (i.e.,
 they are different and they share an edge or a corner).
 The length of a clear path is the number of visited cells of this path.
 
- 
+
 
 Example 1:
 
@@ -27,7 +27,7 @@ Example 3:
 
 Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
 Output: -1
- 
+
 
 Constraints:
 
@@ -42,26 +42,27 @@ from collections import OrderedDict, deque
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
         """
-        :type grid: List[List[int]]
-        :rtype: int
-[[0,1,0,0,0,0],[0,1,0,1,1,0],[0,1,1,0,1,0],[0,0,0,0,1,0],[1,1,1,1,1,0],[1,1,1,1,1,0]]
-[[0,0,0],[1,1,0],[1,1,0]]
-[[1,0,0],[1,1,0],[1,1,0]]
-[[0,0,0],[1,0,0],[1,1,0]]
-[[0,1],[1,0]]
-[[1]]
-[[0]]
-        
-        
-        
-        [0,1,0,0,0,0]
-        [0,1,0,1,1,0]
-        [0,1,1,0,1,0]
-        [0,0,0,0,1,0]
-        [1,1,1,1,1,0]
-        [1,1,1,1,1,0]
+                :type grid: List[List[int]]
+                :rtype: int
+        [[0,1,0,0,0,0],[0,1,0,1,1,0],[0,1,1,0,1,0],[0,0,0,0,1,0],[1,1,1,1,1,0],[1,1,1,1,1,0]]
+        [[0,0,0],[1,1,0],[1,1,0]]
+        [[1,0,0],[1,1,0],[1,1,0]]
+        [[0,0,0],[1,0,0],[1,1,0]]
+        [[0,1],[1,0]]
+        [[1]]
+        [[0]]
+
+
+
+                [0,1,0,0,0,0]
+                [0,1,0,1,1,0]
+                [0,1,1,0,1,0]
+                [0,0,0,0,1,0]
+                [1,1,1,1,1,0]
+                [1,1,1,1,1,0]
         """
-        if grid[0][0] != 0 or grid[-1][-1] != 0: return -1
+        if grid[0][0] != 0 or grid[-1][-1] != 0:
+            return -1
         n = len(grid)
         dq = deque()
         dq.append((0, 0))
@@ -70,33 +71,53 @@ class Solution(object):
         while dq:
             i, j = dq.popleft()
             # print(repo)
-            level = repo[(i, j)]     
-            if i+1<n and grid[i+1][j] == 0 and (i+1, j) not in repo:
-                dq.append((i+1, j))
-                repo[(i+1, j)] = level + 1
-            if i+1<n and j+1 <n and grid[i+1][j+1] == 0 and (i+1, j+1) not in repo:
-                dq.append((i+1, j+1))
-                repo[(i+1, j+1)] = level + 1
-            if i+1<n and j-1>=0 and grid[i+1][j-1] == 0 and (i+1, j-1) not in repo:
-                dq.append((i+1, j-1))
-                repo[(i+1, j-1)] = level + 1
-            if j+1<n and grid[i][j+1] == 0 and (i, j+1) not in repo:
-                dq.append((i, j+1))
-                repo[(i, j+1)] = level + 1
-            if i-1>=0 and grid[i-1][j] == 0 and (i-1, j) not in repo:
-                dq.append((i-1, j))
-                repo[(i-1, j)] = level + 1
-            if i-1>=0 and j+1 <n and grid[i-1][j+1] == 0 and (i-1, j+1) not in repo:
-                dq.append((i-1, j+1))
-                repo[(i-1, j+1)] = level + 1
-            if i-1>=0 and j-1>=0 and grid[i-1][j-1] == 0 and (i-1, j-1) not in repo:
-                dq.append((i-1, j-1))
-                repo[(i-1, j-1)] = level + 1
-            if j-1>=0 and grid[i][j-1] == 0 and (i, j-1) not in repo:
-                dq.append((i, j-1))
-                repo[(i, j-1)] = level + 1
+            level = repo[(i, j)]
+            if i + 1 < n and grid[i + 1][j] == 0 and (i + 1, j) not in repo:
+                dq.append((i + 1, j))
+                repo[(i + 1, j)] = level + 1
+            if (
+                i + 1 < n
+                and j + 1 < n
+                and grid[i + 1][j + 1] == 0
+                and (i + 1, j + 1) not in repo
+            ):
+                dq.append((i + 1, j + 1))
+                repo[(i + 1, j + 1)] = level + 1
+            if (
+                i + 1 < n
+                and j - 1 >= 0
+                and grid[i + 1][j - 1] == 0
+                and (i + 1, j - 1) not in repo
+            ):
+                dq.append((i + 1, j - 1))
+                repo[(i + 1, j - 1)] = level + 1
+            if j + 1 < n and grid[i][j + 1] == 0 and (i, j + 1) not in repo:
+                dq.append((i, j + 1))
+                repo[(i, j + 1)] = level + 1
+            if i - 1 >= 0 and grid[i - 1][j] == 0 and (i - 1, j) not in repo:
+                dq.append((i - 1, j))
+                repo[(i - 1, j)] = level + 1
+            if (
+                i - 1 >= 0
+                and j + 1 < n
+                and grid[i - 1][j + 1] == 0
+                and (i - 1, j + 1) not in repo
+            ):
+                dq.append((i - 1, j + 1))
+                repo[(i - 1, j + 1)] = level + 1
+            if (
+                i - 1 >= 0
+                and j - 1 >= 0
+                and grid[i - 1][j - 1] == 0
+                and (i - 1, j - 1) not in repo
+            ):
+                dq.append((i - 1, j - 1))
+                repo[(i - 1, j - 1)] = level + 1
+            if j - 1 >= 0 and grid[i][j - 1] == 0 and (i, j - 1) not in repo:
+                dq.append((i, j - 1))
+                repo[(i, j - 1)] = level + 1
 
-        if (n-1, n-1) not in repo:
+        if (n - 1, n - 1) not in repo:
             return -1
         else:
-            return repo[(n-1, n-1)]
+            return repo[(n - 1, n - 1)]
