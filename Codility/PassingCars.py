3b2e46924b75b7ec9164b0bd97e81736f7e16357
Cheedge <cheedgelee@gmail.com>
2022-09-05 12:22:30 +0200

    Counter

---
+++ Codility/PassingCars.py
@@ -0,0 +1,94 @@
+"""
+A non-empty array A consisting of N integers is given.
+The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q),
+where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+
+"""
+from collections import Counter
+from typing import List
+
+import pytest
+
+
+# O(N**2)
+def solution(A):
+    # write your code in Python 3.6
+    n = len(A)
+    res = 0
+    for i in range(n):
+        if A[i] == 0:
+            for j in range(i + 1, n):
+                if A[j] == 1:
+                    res += 1
+    return res
+
+
+def passing_cars(A: List[int]) -> int:
+    res = 0
+    cnt = Counter(A)
+    for i in range(len(A)):
+        if A[i] == 0:
+            cnt[0] -= 1
+            res += cnt[1]
+        else:
+            cnt[1] -= 1
+    if res > 1000000000:
+        res = -1
+    return res
+
+
+test_data = [
+    ([0, 1, 0, 1, 0], 3),
+    ([0, 1, 0, 1, 1], 5),
+    ([0], 0),
+    ([1], 0),
+]
+
+
+@pytest.mark.parametrize("A, expect", test_data)
+def test_solution(A, expect):
+    res = solution(A)
+    assert res == expect
+
+
+@pytest.mark.parametrize("A, expect", test_data)
+def test_passing_cars(A, expect):
+    res = passing_cars(A)
+    assert res == expect
