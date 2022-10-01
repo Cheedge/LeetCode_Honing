81ec31ae2bce090da3fae78fd50585afbaa3ce8b
Cheedge <cheedgelee@gmail.com>
2022-10-01 23:49:21 +0200

    recursion

--- Leetcode/617.py
+++ Leetcode/617.py
@@ -4,11 +4,11 @@ Easy
 
 You are given two binary trees root1 and root2.
 
-Imagine that when you put one of them to cover the other, 
-some nodes of the two trees are overlapped while the others are not. 
-You need to merge the two trees into a new binary tree. 
-The merge rule is that if two nodes overlap, 
-then sum node values up as the new value of the merged node. 
+Imagine that when you put one of them to cover the other,
+some nodes of the two trees are overlapped while the others are not.
+You need to merge the two trees into a new binary tree.
+The merge rule is that if two nodes overlap,
+then sum node values up as the new value of the merged node.
 Otherwise, the NOT null node will be used as the node of the new tree.
 
 Return the merged tree.
@@ -22,7 +22,7 @@ Example 2:
 
 Input: root1 = [1], root2 = [1,2]
 Output: [2,2]
- 
+
 
 Constraints:
 
@@ -32,6 +32,7 @@ The number of nodes in both trees is in the range [0, 2000].
 
 from typing import Optional
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -39,8 +40,11 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    def mergeTrees(
+        self, root1: Optional[TreeNode], root2: Optional[TreeNode]
+    ) -> Optional[TreeNode]:
         if not root1:
             return root2
         if not root2:
