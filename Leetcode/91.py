dbe813bee751de58310bc6281e988a5e0683b440
Cheedge <cheedgelee@gmail.com>
2022-10-03 15:03:34 +0200

    DP

--- Leetcode/91.py
+++ Leetcode/91.py
@@ -21,7 +21,7 @@ Given a string s containing only digits, return the number of ways to decode it.
 
 The test cases are generated so that the answer fits in a 32-bit integer.
 
- 
+
 
 Example 1:
 
@@ -38,13 +38,15 @@ Example 3:
 Input: s = "06"
 Output: 0
 Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
- 
+
 
 Constraints:
 
 1 <= s.length <= 100
 s contains only digits and may contain leading zero(s).
 """
+
+
 class Solution(object):
     def numDecodings(self, s):
         """
@@ -54,25 +56,60 @@ class Solution(object):
         n = len(s)
         # if initialized as 0, in some case, cannot distinguish returned 0 and orignal 0.
         dp = [-1 for i in range(n)]
-        
+
         def DFS(s, i, dp):
             # reach end will return 1: 1 way to reach to end
-            if i>=n: return 1
+            if i >= n:
+                return 1
             # 0 will return 0: 0 way to reach to end
-            if s[i] == "0": return 0
+            if s[i] == "0":
+                return 0
             # check dp
-            if dp[i]!=-1: return dp[i]
+            if dp[i] != -1:
+                return dp[i]
             # recursion
             # 1. single digit
-            if 0<int(s[i])<=9:
-                dp[i] = DFS(s, i+1, dp)
+            if 0 < int(s[i]) <= 9:
+                dp[i] = DFS(s, i + 1, dp)
             # 2. two digits: 10<=s[i-1:i+1]<=26
-            if i<n-1 and eval(s[i]+s[i+1]+'<=26'):
-                dp[i] += DFS(s, i+2, dp)
+            if i < n - 1 and eval(s[i] + s[i + 1] + "<=26"):
+                dp[i] += DFS(s, i + 2, dp)
             print(dp)
             return dp[i]
-        
+
         return DFS(s, 0, dp)
         # dp[i]: before i, the number of ways to decode
         # 1. nums[i]!=0: return 0
         # 2. 'nums[i-1]+nums[i]<26'
+
+    def numDecodings1(self, s: str) -> int:
+        # DFS
+        # dp[i]: how many decode ways of s[i:] NOTICE!!! from back to front
+        n = len(s)
+        dp = [0] * n
+
+        def DFS(i: int, dp: list) -> int:
+            if i >= n:
+                return 1
+            if dp[i] != 0:
+                return dp[i]
+            # 1 digit
+            #   1-9; else 0
+            if 0 < int(s[i]) < 10:
+                # print(i,s[i], dp)
+                dp[i] = DFS(i + 1, dp)
+            else:
+                return 0
+            # 2 digits
+            #   10-26
+            if i < n - 1 and 10 <= int(s[i : i + 2]) <= 26:
+                # print(i,s[i:i+2], dp)
+                dp[i] += DFS(i + 2, dp)
+            # else:
+            #     return 0
+            # print(f"{i=}",dp)
+            return dp[i]
+
+        # return DFS(0, dp)
+        DFS(0, dp)
+        return dp[0]
